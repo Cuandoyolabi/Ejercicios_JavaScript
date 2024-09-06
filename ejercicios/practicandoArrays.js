b902f152f991d9dd49d21aeabf3724fc7836fa78
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-09-06 13:52:12 -0600

    "Practicando metodos de busqueda y filtrado"

--- ejercicios/practicandoArrays.js
+++ ejercicios/practicandoArrays.js
@@ -1,6 +1,8 @@
 //Practicare Metodos para agregar y Eliminar Objetos.  //60 EJEMPLOS
 
 //Push Agrega elemento a final del array
+
+/*
 let array_n1 = [1,2,3,4,5,6,7,8,9,10];
 
 console.log(array_n1);
@@ -60,4 +62,104 @@ const obj_n1 = {
     c: "C"
 };
 
-array_n2
+array_n1.unshift(obj_n1);
+console.log(array_n1);
+array_n1.unshift(2);
+array_n1.pop();
+array_n1.pop();
+console.log(array_n1);
+
+//Shift elimina el primer elemento de un array a diferencia de pop() que elimina el ultimo
+console.log(array_n1.lenght);
+array_n1.shift();
+array_n1.shift();
+array_n1.shift();
+console.log(array_n1.length);
+array_n1.unshift(obj_n1);
+console.log(array_n1.length);
+console.log(array_n1);
+array_n1.unshift(array_n2);
+console.log(array_n1.length);
+array_n1.shift();
+array_n1.shift();
+array_n1.shift();
+array_n1.shift();
+console.log(array_n1.length);
+array_n1.shift();
+array_n1.shift();
+array_n1.shift();
+console.log(array_n1.length);
+console.log(array_n1);
+
+/*
+
+//Splice (Inicio, cantidad, item1, item2)
+// array.splice(start,deleteCount, item1, item2);
+/*
+start: Índice en el que comienza la modificación del arreglo. Es obligatorio.
+deleteCount: Número de elementos a eliminar desde el índice start. Si es 0, no se elimina ningún elemento. Este parámetro es opcional.
+item1, item2, ..., itemN: Elementos que se agregan en el lugar especificado. Este parámetro es opcional.
+*/
+
+//Splice pide primero un indice, luego numero de elementos a eliminar, luego elementos que quieres agregar
+
+/*
+
+const array_n3 = [1,2,3,4,5,"Hola","Perro","Gato","Cuervo"];
+
+//Eliminar elementos
+array_n3.splice(2,1);
+console.log(array_n3);
+
+//Agregar elementos
+array_n3.splice(2, 0, "A", "B", "C");
+console.log(array_n3);
+
+array_n3.splice(11, 0, "Rana");
+console.log(array_n3);
+
+//Remplazar elementos
+array_n3.splice(11, 1, "Vaca");
+console.log(array_n3);
+
+array_n3.splice(0, 2, 1240, 1300);
+console.log(array_n3);
+
+array_n3.splice(0,1,true);
+array_n3.splice(1,1,false);
+
+let array_n4 = [183746587345];
+array_n3.unshift(array_n4);
+console.log(array_n3);
+let array_n5 = [582645837456];
+array_n3.splice(0,1, array_n5);
+console.log(array_n3);
+
+//Slice te retorna una pieza de un array de inicio a fin sin modificar el array original
+const primerPieza = array_n3.slice(3,6);
+console.log(primerPieza);
+const segundaPieza = array_n3.slice(8,13);
+console.log(segundaPieza);
+const terceraPieza = array_n3.slice(1,3);
+console.log(terceraPieza);
+const cuartaPieza = array_n3.slice(6, 8);
+console.log(cuartaPieza);
+const quintaPieza = array_n3.slice(10, 12);
+console.log(quintaPieza);
+const sextaPieza = array_n3.slice(0,9);
+console.log(sextaPieza);
+const septimaPieza = array_n3.slice(0,2);
+console.log(septimaPieza);
+const octavaPieza = array_n3.slice(11, 14);
+console.log(octavaPieza);
+const novenaPieza = array_n3.slice(3, 5);
+console.log(novenaPieza);
+const decimaPieza = array_n3.slice(8, 10);
+console.log(decimaPieza);
+*/
+
+//Practicando Metodos para busqueda y filtrado 80 EJEMPLOS
+let array_n5 = [true,false,"abcdefghi",5,6,5,10, "ABC"];
+
+
+
