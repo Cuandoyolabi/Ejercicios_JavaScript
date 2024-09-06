7b08993a25bd790b930220b5eb4127e2eac1bbb6
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-09-06 12:24:25 -0600

    " Practicando con sets"

--- ejercicios/objetosConPrototipos.js
+++ ejercicios/objetosConPrototipos.js
@@ -1,1 +1,40 @@
-C
+//Constructor base
+function DispositivoElectronico(marca){
+    this.marca = marca;
+};
+
+//Metodo prender
+DispositivoElectronico.prototype.encender = function(){
+    console.log(`El dispositivo de marca ${this.marca} esta encendido`);
+};
+
+//Metodo apagar
+DispositivoElectronico.prototype.apagar = function(){
+    console.log(`El dispositivo de marca ${this.marca} esta apagado`);
+};
+
+//Herencia del Prototipo
+function TelefonoMovil(marca, modelo){
+    DispositivoElectronico.call(this, marca);
+    this.modelo = modelo;
+};
+
+TelefonoMovil.prototype = Object.create(DispositivoElectronico.prototype);
+TelefonoMovil.prototype.constructor = TelefonoMovil;
+
+//Metodo llamar
+TelefonoMovil.prototype.hacerLlamada = function(numero){
+    console.log(`Llamando al ${numero} desde ${this.marca} ${this.modelo}`);
+};
+
+//Metodo tomarFoto
+TelefonoMovil.prototype.tomarFoto = function(){
+    console.log(`Tomando una foto con ${this.marca} ${this.modelo}`);
+};
+
+const miTelefono = new TelefonoMovil('Samsung', 'Galaxy S21');
+miTelefono.encender();
+miTelefono.hacerLlamada();
+miTelefono.tomarFoto();
+miTelefono.apagar();
+
---
+++ ejercicios/programaQueBusca.js
@@ -0,0 +1,34 @@
+//Programa que busque valores en un array con diferentes metodos
+//1-For of
+//2-ForEach()
+//3-Filter()
+//5-Set
+
+//Arrays
+let tecnologias = ["Html","Css","Sass","JavaScript","React","Node.js","Vue.js","Angular","Bootstrap"];
+
+//For in es para objetos
+//For of es para arrays,strings,mapas,sets
+//ForEach es especifico para arrays 
+//Set es un objeto, en el que no se le pueden repetir valores
+
+//For of
+for(let i of tecnologias){
+    i += "a";
+    console.log(i);
+};
+
+//ForEach
+tecnologias.forEach((valor) => {
+    valor += "a";
+    console.log(valor);
+});
+
+//Filter
+const masDeCinco = tecnologias.filter((palabra) => palabra.length > 6);
+console.log(masDeCinco);
+
+//Set
+
+
+
